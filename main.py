ae4ea80784a3ee5a28d25894cccc4cc73164040a
betanec <75495650+betanec@users.noreply.github.com>
2022-06-14 17:48:36 +0300

    example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lib.Model import Agregate
 
-file = r"C:\it\way\ТЗ, макрос от программиста.xlsx"
+file = r".\example\ТЗ, макрос от программиста.xlsx"
 
 if __name__ == '__main__':
     app = Agregate(file=file)
