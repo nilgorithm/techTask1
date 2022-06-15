41e3d5154a7b359748c22a07e52cda2ab22925df
betanec <75495650+betanec@users.noreply.github.com>
2022-06-15 14:59:11 +0300

    alinas way

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from lib.Model import Agregate
 file = r".\example\ТЗ, макрос от программиста.xlsx"
 delimiter = ',' #delimiter for csv
 
+# file = r"C:\Users\User\OneDrive\Рабочий стол\RR\отток бизнеса\РБ\отток бизнеса RR.xlsx"
+# delimiter = ';' #delimiter for csv
+
 if __name__ == '__main__':
     app = Agregate(file=file, delim_csv=delimiter)
     app.result
